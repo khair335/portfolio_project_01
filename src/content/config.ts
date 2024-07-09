37f0dd2ad93bf473c71044f0dbf4ef9620e13231
khair335 <abulkhair6363@gmail.com>
2024-07-09 10:44:51 +0600

    image fixes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  schema: z.object({
+  schema:(image)=> z.object({
     title: z.string(),
     pubDate: z.date(), // Automatically parses ISO string to Date object
     pubTime: z.number(), // Assuming this is intended to represent time in minutes
