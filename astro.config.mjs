220726e4ba8bad78021c33773d35684d4706fc52
khair335 <abulkhair6363@gmail.com>
2024-07-10 00:22:48 +0600

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig, squooshImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-
+import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   service: squooshImageService(),
-     output: "server",
+  output: "server",
+      adapter: vercel(),
 });
