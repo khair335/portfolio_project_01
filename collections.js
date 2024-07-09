5a84307edde549997999f6a5b8fdb2e92767cb63
khair335 <abulkhair6363@gmail.com>
2024-07-09 23:56:35 +0600

    pagination  fixes

--- collections.js
+++
@@ -1,7 +0,0 @@
-// collections.js (or within your .astro component)
-export const collections = {
-  blog: {
-    path: './pages/*.md',
-    // You can add other options like sort, reverse, etc. here
-  }
-};
