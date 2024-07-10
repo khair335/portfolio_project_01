06be38378674ae1042a8ee3e96a49ea9a6e70f8d
khair335 <abulkhair6363@gmail.com>
2024-07-10 09:58:57 +0600

    passthroughImageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import { imageService } from "@unpic/astro/service";
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
@@ -7,9 +7,10 @@ import netlify from "@astrojs/netlify/functions";
 import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-   service: squooshImageService(),
-  output: "hybrid",
-
+   service: passthroughImageService(),
+  output: "server",
+  adapter: vercel(),
+  adapter: netlify(),
   adapter: cloudflare({
     mode: 'directory', // Or 'advanced'
     compatibilityFlags: ['nodejs_compat']
